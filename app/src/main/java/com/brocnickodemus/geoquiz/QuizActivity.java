e93f81267ed08da63310b8484c73995087e72f19
Broc Nickodemus <nickodeb@oregonstate.edu>
2017-10-08 15:56:09 -0700

    ch5 added p98 and 99

--- app/src/main/java/com/brocnickodemus/geoquiz/QuizActivity.java
+++ app/src/main/java/com/brocnickodemus/geoquiz/QuizActivity.java
@@ -16,6 +16,7 @@ public class QuizActivity extends AppCompatActivity {
     private Button mTrueButton;
     private Button mFalseButton;
     private Button mNextButton;
+    private Button mCheatButton;
     private TextView mQuestionTextView;
 
     private Question[] mQuestionBank = new Question[] {
@@ -63,6 +64,14 @@ public class QuizActivity extends AppCompatActivity {
             public void onClick(View v) {
                 mCurrentIndex = (mCurrentIndex + 1) % mQuestionBank.length;
                 updateQuestion();
+            }
+        });
+
+        mCheatButton = (Button)findViewById(R.id.cheat_button);
+        mCheatButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Start CheatActivity
             }
         });
 
